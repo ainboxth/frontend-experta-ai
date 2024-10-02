f96fc1da6af8ddbf886a8c352c854045588f4fbc
Pattanasak <Putjat145@gmail.com>
2024-10-02 23:00:03 +0700

    feat: add develop space

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,11 +1,11 @@
 export const ConfigPage = {
 
-    // //============== Development ================
-    // URL_WORK_IMG: `src/images/img2img`,
-    // URL_BACKEND: "http://localhost:3000",
+    //============== Development ================
+    URL_WORK_IMG: `src/images/img2img`,
+    URL_BACKEND: "https://boar-magnetic-happily.ngrok-free.app/",
 
     
-    //============== production =====================
-    URL_WORK_IMG: "src/images/img2img",
-    URL_BACKEND: "https://mistakenly-in-panther.ngrok-free.app"
+    // //============== production =====================
+    // URL_WORK_IMG: "src/images/img2img",
+    // URL_BACKEND: "https://mistakenly-in-panther.ngrok-free.app"
 }
